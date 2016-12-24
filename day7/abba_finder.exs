2cb82044588d8c0b4c9618df06d0bdd2c62e84d9
Mike Waldner <mwaldner@surgeforward.com>
2016-12-24 14:53:46 -0700

    I had a terrible answer. Looks at some other solutions for better direction
    String pattern matching is really cool.

--- day7/abba_finder.exs
+++ day7/abba_finder.exs
@@ -1,7 +1,35 @@
 defmodule AbbaFinder do
   def find(input) do
-    captures = Regex.named_captures(~r/(?<first>.*)\[(?<second>.+)\](?<third>.*)/, encrypted_sector)
+    input
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(fn ip -> tls?(ip) end)
+    |> Enum.count(fn is_tls -> is_tls end)
   end
+
+  defp tls?(ip) do
+    sequences = ip |> String.trim |> String.split(~r{[\[\]]})
+
+    ip_parts = Enum.take_every(sequences, 2)
+    hypernets = sequences |> Enum.drop(1) |> Enum.take_every(2)
+
+    ip_contains_abba =
+      ip_parts
+      |> Enum.map(fn ip -> abba?(ip) end)
+      |> Enum.any?
+
+    hypernets_contains_abba =
+      hypernets
+      |> Enum.map(fn ip -> abba?(ip) end)
+      |> Enum.any?
+
+    ip_contains_abba and not hypernets_contains_abba
+  end
+
+  # Super cool string pattern matching from JEG2
+  defp abba?(sequence) when byte_size(sequence) < 4, do: false
+  defp abba?(<<a::utf8, b::utf8, b::utf8, a::utf8, _sequence::binary>>) when a != b, do: true
+  defp abba?(<<_char::utf8, sequence::binary>>), do: abba?(sequence)
 end
 
 System.argv
